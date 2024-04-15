81b69736d58833529c7a83cfa1eb701b552855af
bitfexl <54662051+bitfexl@users.noreply.github.com>
2024-04-15 12:54:02 +0200

    added tailwindcss

--- frontend/schnopsn/src/App.tsx
+++ frontend/schnopsn/src/App.tsx
@@ -4,9 +4,13 @@ export default function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <div>
-            <h1>Schnopsn</h1>
-            <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <div className="flex flex-col gap-6 m-10 mx-60">
+            <h1 className="text-3xl border-b-2">Schnopsn</h1>
+            <div>
+                <button onClick={() => setCount((count) => count + 1)} className="border-2 border-gray-600 px-2">
+                    count is {count}
+                </button>
+            </div>
         </div>
     );
 }
