0de3195f795fe1b1395d6ed412b67811ca4d8d85
bitfexl <54662051+bitfexl@users.noreply.github.com>
2024-04-17 21:34:21 +0200

    started game serializer

--- backend/schnopsn/src/main/java/com/github/bitfexl/schnopsn/game/GameSerializer.java
+++ backend/schnopsn/src/main/java/com/github/bitfexl/schnopsn/game/GameSerializer.java
@@ -1,2 +1,19 @@
-package com.github.bitfexl.schnopsn.game;public class GameSerializer {
+package com.github.bitfexl.schnopsn.game;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class GameSerializer {
+    private final GameRound game;
+
+    public void serializePlayer1() {
+        game.getFirstPlayerState();
+
+        game.getTrumpfColor();
+        game.getTrumpfCard();
+        game.getPlayedCard();
+
+        game.getSecondPlayerState().getStiche().getFirst();
+        game.getSecondPlayerState().getCards().size();
+    }
 }
